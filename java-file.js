aee325f08347cc833e623d5c0a0a2f1bd9e295e4
kakisaura <82690252+kakisaura@users.noreply.github.com>
2022-12-09 18:54:39 -0300

    Create a basic layout

---
+++ java-file.js
@@ -0,0 +1,40 @@
+const sum = function sum(x, y) {
+    return x + y;
+}
+
+const subtract = function subtract(x, y) {
+    return x - y;
+}
+
+const multiply = function multiply(x, y) {
+    return x * y;
+}
+
+const divide = function division(x, y) {
+    return x / y;
+}
+console.log(divide(5, 7));
+
+
+//Buttons
+const btnOne = document.querySelector('.one');
+const btnTwo = document.querySelector('.two');
+const btnThree = document.querySelector('.three');
+const btnFour = document.querySelector('.four');
+const btnFive = document.querySelector('.five');
+const btnSix = document.querySelector('.six');
+const btnSeven = document.querySelector('.seven');
+const btnEight = document.querySelector('.eight');
+const btnNine = document.querySelector('.nine');
+const btnZero = document.querySelector('.zero');
+const btnDot = document.querySelector('.dot');
+const btnEqual = document.querySelector('.Equal');
+const btnAdd = document.querySelector('.addition');
+const btnMinus = document.querySelector('.subtraction');
+const btnMultiply = document.querySelector('.multiplication');
+const btnDivide = document.querySelector('.division');
+const btnErase = document.querySelector('.backspace');
+const btnClear = document.querySelector('.clear');
+
+console.log(btnOne);
+
